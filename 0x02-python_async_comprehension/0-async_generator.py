e57856e5479a07f6dc786503f8be6b1ab33e0970
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-16 20:08:43 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,8 +9,7 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """ 
-    Asynchronously generates a sequence of 10 random float numbers in the range [0, 10].
-    Each number is generated after a delay of 1 second.
+    Asynchronously generates after a delay of 1 second.
     """
     for _ in range(10):
         await sleep(1)
